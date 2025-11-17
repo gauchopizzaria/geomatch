13c87cbffffad9767d32ad58020002b51871650a
FelipeSysten <systen3@gmail.com>
2025-11-17 18:25:05 -0300

    correção para atualizar foto

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :r2
 
-  config.active_storage.service_urls_expire_in = 1.week
+ config.active_storage.resolve_model_to_route = :public
+ config.active_storage.service_urls_expire_in = nil
   
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
