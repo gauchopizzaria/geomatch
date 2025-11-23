ea289e47ae4ac9b508e64519e5e80b30ed54c64c
gauchopizzaria <admin@gauchocafeepizzaria.com>
2025-11-23 13:23:08 -0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  Rails.application.routes.default_url_options[:host] = "https://geomatch-web.onrender.com"
+  Rails.application.routes.default_url_options[:host] = "https://geomatch-cvtv.onrender.com"
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
