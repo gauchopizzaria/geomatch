8e4c26aa26e77ec2877f3e3c318c97ef1db42a5e
André Alves <alves.andre.ds@gmail.com>
2026-01-28 18:19:34 -0300

    Add CheckoutPreferenceBuilder service for MercadoPago integration; implement preference building logic with item details, payer information, and webhook handling

---
+++ app/services/mercado_pago/checkout_preference_builder.rb
@@ -0,0 +1,84 @@
+module MercadoPago
+  class CheckoutPreferenceBuilder
+    def initialize(payment:, user:, plan:)
+      @payment = payment
+      @user = user
+      @plan = plan
+    end
+
+    def call
+      build_preference
+    end
+
+    private
+
+    attr_reader :payment, :user, :plan
+
+    def build_preference
+      {
+        items: [
+          {
+            id: plan.id.to_s,
+            title: plan.name,
+            description: plan.description,
+            quantity: 1,
+            currency_id: plan.price_currency,
+            unit_price: plan.price.to_f
+          }
+        ],
+        payer: {
+          email: User.first.email,
+          name: user.username
+        },
+        external_reference: payment.id,
+        notification_url: webhook_url,
+        back_urls: {
+          success: success_checkouts_url,
+          failure: failure_checkouts_url,
+          pending: pending_checkouts_url
+        },
+        auto_return: "approved",
+        binary_mode: true,
+        statement_descriptor: "GEOMATCH",
+        metadata: {
+          payment_id: payment.id,
+          user_id: user.id,
+          plan_id: payment.plan_id
+        },
+        expires: true,
+        expiration_date_from: expiration_date_from,
+        expiration_date_to: expiration_date_to
+      }
+    end
+
+    def base_url
+      "https://b84ff9939151.ngrok-free.app"
+      #ENV['APP_BASE_URL'].presence ||
+      #  Rails.application.routes.default_url_options[:host].presence
+    end
+
+    def webhook_url
+      "#{base_url.to_s.delete_suffix("/")}/webhooks/mercado_pago"
+    end
+
+    def expiration_date_from
+      Time.current.iso8601
+    end
+
+    def expiration_date_to
+      (Time.current + 1.hour).iso8601
+    end
+
+    def success_checkouts_url
+      "#{base_url}/discover"
+    end
+
+    def failure_checkouts_url
+      "#{base_url}/plans"
+    end
+
+    def pending_checkouts_url
+      "#{base_url}/discover"
+    end
+  end
+end
