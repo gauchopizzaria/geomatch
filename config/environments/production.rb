590d433a01ba377e6113f0570f62dff4aaddba7e
FelipeSysten <systen3@gmail.com>
2026-02-12 12:15:56 -0300

    feat:recuperar senha

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,18 @@ Rails.application.configure do
   # --------------------------
   config.action_mailer.default_url_options = { host: "geomatch-cvtv.onrender.com" }
 
+  # Configurações SMTP para envio de e-mails em produção
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_ADDRESS'],
+    port:                 ENV['SMTP_PORT'],
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    domain:               'geomatchbr.com' # Substitua pelo seu domínio
+  }
+
   # --------------------------
   # I18n fallback
   # --------------------------
