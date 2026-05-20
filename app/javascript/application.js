68d1bbc3d198638701c65c273881235fa255ef87
Felipe <systen3@gmail.com>
2026-05-20 14:58:38 -0300

    modo claro e escuro

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,6 @@ import "./chat_list_logic";
 import "./stories";
 import "trix";
 import "@rails/actiontext";
-import "./dark_mode_toggle";
 import "./push_notifications"; // Importa o arquivo de notificações push
 import "./live_location"; // Importa o módulo de rastreamento contínuo
 import "./map_3d"; // Importa o módulo do mapa 3D (Mapbox GL JS)
