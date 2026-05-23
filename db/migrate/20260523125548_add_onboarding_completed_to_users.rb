b5723cc2973a401f34e73c0bfd01872514bc6944
Felipe <systen3@gmail.com>
2026-05-23 18:48:59 -0300

    botoes claro e escuro

--- db/migrate/20260523125548_add_onboarding_completed_to_users.rb
+++ db/migrate/20260523125548_add_onboarding_completed_to_users.rb
@@ -1,5 +1,5 @@
-class AddOnboardingCompletedToUsers < ActiveRecord::Migration[8.1]
-  def change
-    add_column :users, :onboarding_completed, :boolean, default: false, null: false
+  class AddOnboardingCompletedToUsers < ActiveRecord::Migration[8.1]
+    def change
+      add_column :users, :onboarding_completed, :boolean, default: false, null: false
+    end
   end
-end
